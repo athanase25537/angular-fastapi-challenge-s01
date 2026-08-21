60a931f04d1297bf5226804280aed03acb2da4e1
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-08-21 16:33:49 +0300

    feat: update routing to include DashboardComponent and enhance link styles

--- frontend/DTOGenerator/src/app/app.routes.ts
+++ frontend/DTOGenerator/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard-component/dashboard-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "dashboard", component: DashboardComponent },
+    // { path: "/transactions", component: }
+];
