5e45e3e9c50c17c9068b8afe905080bd355f156c
athanase25537 <andriamasyathanase.nazy@gmail.com>
2026-08-24 15:57:12 +0300

    feat: add dotenv dependency and implement database connection setup

---
+++ backend/core/database.py
@@ -0,0 +1,29 @@
+from os import getenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
+from typing import Annotated
+from fastapi import Depends 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = getenv("DATABASE_URL", "")
+print(DATABASE_URL)
+
+engine = create_engine(url=DATABASE_URL)
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
+    
+def get_db():
+    db = SessionLocal()
+    
+    try:
+        yield db
+    finally:
+        db.close()
+        
+db_dependency = Annotated[Session, Depends(get_db)]
